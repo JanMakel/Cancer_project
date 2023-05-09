0ae3b410b140d720943c45f1d86fbce8cc9aa5e3
JanMakel <JanMakel02@gmail.com>
2023-05-09 06:35:30 +0200

    Movimiento

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,41 +13,78 @@ public class PlayerController : MonoBehaviour
     private float rotationSpeed = 30f;
     [SerializeField]private float playerLive;
     private bool gameOver;
-
     private int spores;
 
-    
+    //Movimiento
+    public float velocity = 5f;
+    public float turnspeed = 10f;
+
+    Vector2 input;
+    float angle;
+    Quaternion targetrotation;
+    Transform cam; 
+
+
+
+
     void Start()
     {
+        cam = Camera.main.transform;
         _rigidbody = GetComponent<Rigidbody>();
         playerLive = 3;
         gameOver = false;
         spores = 0;
     }
 
-    
+    //Funcion que coge el eje horizontal con a,d,< o >, y el eje vertical w, s, ^, v
+    private void GetInput()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    //Direccion relativa a la rotacion de la camara
+    private void CalculateRotation()
+    {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+            
+    }
+
+
+    //Rotacion conforme al angulo calculado
+    private void rotate()
+    {
+        targetrotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetrotation, turnspeed * Time.deltaTime); 
+    }
+
+    //El jugador se movera siempre hacia su adelante
+    private void Move()
+    {
+        transform.position += transform.forward * velocity * Time.deltaTime;
+    }
+
+
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        _rigidbody.AddForce(transform.forward * speed * verticalInput);
+        GetInput();
+
+        if (Mathf.Abs(input.x) < 1 && Mathf.Abs(input.y) < 1) return;
+
+        CalculateRotation();
+        rotate();
+        Move();
+
 
         
-         if (Mathf.Approximately(verticalInput, 0))
-        {
-            _rigidbody.velocity = Vector3.zero;
-        }
-
-        if( Mathf.Approximately(horizontalInput,0))
-        {
-            _rigidbody.angularVelocity = Vector3.zero;
-        }
-        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+      
         if (Input.GetKeyDown(KeyCode.Space))
         {
             jump();
         }
-
+        
         
     }
 
